51999cb7398df3ff0b40802539b076c821ba834b
Tobias Reize <reizeto@web.de>
2025-06-11 10:08:29 +0200

    updated LoginComponent, added helper.scss and distance.scss, updated some style

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  onSubmit(ngForm: NgForm) {
+    console.log(ngForm.value);
+    ngForm.resetForm();
+  }
 
 }
