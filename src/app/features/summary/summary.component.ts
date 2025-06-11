fc1a74a3d51d9c02430bc9d23d6c72621d1bf259
Tobias Reize <reizeto@web.de>
2025-06-11 14:10:18 +0200

    updated SummaryComponent and some style, added back.png

--- src/app/features/summary/summary.component.ts
+++ src/app/features/summary/summary.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-summary',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss'
 })
 export class SummaryComponent {
+
+  private location = inject(Location);
+  selectedDate: string = '';
+
+
+  goBack() {
+    this.location.back();
+  }
 
 }
