bba1265f16a6cf62d001b27181eb85a0024662bf
Tobias Reize <reizeto@web.de>
2025-06-11 15:33:19 +0200

    added PageNotFoundComponent and updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
 import { CheckInComponent } from './features/check-in/check-in.component';
 import { SummaryComponent } from './features/summary/summary.component';
+import { PageNotFoundComponent } from './features/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'check-in', component: CheckInComponent },
     { path: 'summary', component: SummaryComponent },
+    { path: '**', component: PageNotFoundComponent },
 ];
