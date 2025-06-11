954e47941ac982b56e8e5dfe1fb48911040386b0
Tobias Reize <reizeto@web.de>
2025-06-11 11:52:45 +0200

    added icons, updated CheckInComponent and some style

--- src/app/features/check-in/check-in.component.ts
+++ src/app/features/check-in/check-in.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-check-in',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './check-in.component.html',
   styleUrl: './check-in.component.scss'
 })
 export class CheckInComponent {
+
+  state: 'play' | 'stop' = 'stop';
 
 }
