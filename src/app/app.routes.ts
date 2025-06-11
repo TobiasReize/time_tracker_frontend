3c873ce577a3186cf6b6580f80ff20ec65af7ccf
Tobias Reize <reizeto@web.de>
2025-06-11 10:55:16 +0200

    added CheckInComponent and updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
+import { CheckInComponent } from './features/check-in/check-in.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
+    { path: 'check-in', component: CheckInComponent },
 ];
---
+++ src/app/features/check-in/check-in.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-check-in',
+  standalone: true,
+  imports: [],
+  templateUrl: './check-in.component.html',
+  styleUrl: './check-in.component.scss'
+})
+export class CheckInComponent {
+
+}
